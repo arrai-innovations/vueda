8c074bc5bae375245daecf9f92d7a2ba827a247a
Joel Hillacre <joel@403forbidden.ca>
2024-02-13 09:48:32 -0700

    chore: switch to BSD-3-Clause license.

--- server/vueda/cli.py
+++ server/vueda/cli.py
@@ -1,6 +1,3 @@
-# ══════════════════════════════════════════════════════════════════════════════
-#  Copyright (c) 2023. Arrai Innovations Inc - All Rights Reserved             ═
-# ══════════════════════════════════════════════════════════════════════════════
 import argparse
 import sys
 import termios
--- server/vueda/update.py
+++ server/vueda/update.py
@@ -1,6 +1,3 @@
-# ══════════════════════════════════════════════════════════════════════════════
-#  Copyright (c) 2023. Arrai Innovations Inc - All Rights Reserved             ═
-# ══════════════════════════════════════════════════════════════════════════════
 import argparse
 import os
 import shutil
