de61cfce151a091af36205688503418b028f8b24
Joel Hillacre <joel@403forbidden.ca>
2024-02-15 17:01:50 -0700

    wip: copy in ListRowLevelViewSetMixIn & ContentTypeField.

---
+++ server/vueda/core/fields.py
@@ -0,0 +1,23 @@
+import rest_framework.serializers as drf_serializers
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.encoding import smart_str
+
+
+class ContentTypeField(drf_serializers.RelatedField):
+    default_error_messages = {
+        "does_not_exist": "ContentType with {value} does not exist or is not allowed.",
+        "invalid": "Invalid value.",
+    }
+
+    def to_representation(self, obj):
+        return f"{obj.app_label}/{obj.model}"
+
+    def to_internal_value(self, data):
+        queryset = self.get_queryset()
+        try:
+            app_label, model = data.split("/")
+            return queryset.get(app_label=app_label, model=model)
+        except ObjectDoesNotExist:
+            self.fail("does_not_exist", value=smart_str(data))
+        except (TypeError, ValueError):
+            self.fail("invalid")
--- server/vueda/core/viewsets.py
+++ server/vueda/core/viewsets.py
@@ -1,5 +1,7 @@
 import rest_framework.viewsets as drf_viewsets
 from django.db import transaction
+from django.db.models import Q
+from rest_framework.response import Response
 
 
 class AtomicCreateModelMixin(drf_viewsets.mixins.CreateModelMixin):
@@ -33,3 +35,46 @@ class AtomicModelViewSet(
     drf_viewsets.GenericViewSet,
 ):
     pass
+
+
+class ListRowLevelViewSetMixIn(drf_viewsets.mixins.ListModelMixin, drf_viewsets.GenericViewSet):
+    """
+    A ViewSet mixin that filters out rows that the user does not have access to.
+    """
+
+    def apply_row_level_filter(self, queryset):
+        model = queryset.model
+        row_level_permission = getattr(model, "row_level_permission", None)
+        if row_level_permission is not None:
+            optional_q = row_level_permission.check_queryset(
+                queryset, f"{model._meta.app_label}.list_{model._meta.model_name}", self.request.user, "list"
+            )
+            if isinstance(optional_q, Q):
+                return queryset.filter(optional_q)
+            if optional_q is False:
+                return model.objects.none()
+            # else, optional_q is None, so we don't filter
+            # or optional_q is True, so we don't filter
+            return queryset
+
+    def list(self, request, *args, **kwargs):
+        """
+        applying row level filter in get_queryset() causes problems
+         with other drf actions, specifically encountered with create
+         not finding it's created object
+        """
+        # future: when updating drf, check that the copied code is still the same
+        # code from drf
+        queryset = self.filter_queryset(self.get_queryset())
+        # our addition
+        queryset = self.apply_row_level_filter(queryset)
+        # end addition
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+        # end code from drf
