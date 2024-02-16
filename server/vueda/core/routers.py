34989ce1c02667f3baf08a33cc04c8bfad56ec1c
Joel Hillacre <joel@403forbidden.ca>
2024-02-15 17:08:54 -0700

    wip: IncludeAppInRouteNameRouter.

---
+++ server/vueda/core/routers.py
@@ -0,0 +1,18 @@
+from rest_framework.routers import SimpleRouter
+
+
+class IncludeAppInRouteNameRouter(SimpleRouter):
+    """
+    A router that includes the app name in the route name, in case of model name conflicts between apps.
+    """
+
+    def get_default_basename(self, viewset):
+        queryset = getattr(viewset, "queryset", None)
+
+        assert queryset is not None, (
+            "`basename` argument not specified, and could "
+            "not automatically determine the name from the viewset, as "
+            "it does not have a `.queryset` attribute."
+        )
+
+        return f"{queryset.model._meta.label_lower}"
