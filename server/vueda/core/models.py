5ce99bdda22032a4210a09072a11b405cdf3ae26
Joel Hillacre <joel@403forbidden.ca>
2024-02-16 16:41:39 -0700

    fix: code should be unique on lookup.

--- server/vueda/core/models.py
+++ server/vueda/core/models.py
@@ -12,7 +12,7 @@ class BaseModelMeta:
 
 
 class Lookup(models.Model):
-    code = models.CharField(max_length=255)
+    code = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
 
     class Meta:
