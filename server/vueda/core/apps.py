ba397b14e64d77c36d222b911f6f48e5de3d2e0d
Jayden Kneller <jayden@arrai.com>
2024-02-28 15:05:25 -0700

    fix: Patch django rest framework, so smart_repr doesn't try to access _delegate_text which was removed in django.

--- server/vueda/core/apps.py
+++ server/vueda/core/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     name = "vueda.core"
     verbose_name = "VUEDA Core"
+
+    def ready(self):
+        from vueda.core import rest_framework_smart_repr_patch  # noqa: F401
