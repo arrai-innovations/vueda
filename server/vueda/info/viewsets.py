f7d63b49a60a5ad6e15f33880084970bada2e4b9
Jayden Kneller <jayden@arrai.com>
2024-03-18 17:16:51 -0600

    fix: Move object acquisition into get, so list can return the model names.

--- server/vueda/info/viewsets.py
+++ server/vueda/info/viewsets.py
@@ -10,7 +10,7 @@ from vueda.info.serializers import ModelInfoSerializer
 
 class ModelInfoViewSet(FlexFieldsMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
     """
-    This viewsets is for providing metadata about models, including fields, actions, and permissions
+    This viewset is for providing metadata about models, including fields, actions, and permissions
     to front-end clients. This is a read-only viewset.
 
     You should be able to list all models, and get information about a specific model.
@@ -30,9 +30,9 @@ class ModelInfoViewSet(FlexFieldsMixin, mixins.ListModelMixin, mixins.RetrieveMo
     def get_queryset(self):
         return ContentType.objects.all().filter(pk__in=get_registered_content_types())
 
-    def dispatch(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         self.object = self.get_object(kwargs["app_label"], kwargs["model"])
-        return super().dispatch(request, *args, **kwargs)
+        return super().get(request, *args, **kwargs)
 
     # noinspection PyMethodOverriding
     def get_object(self, app_label, model):
