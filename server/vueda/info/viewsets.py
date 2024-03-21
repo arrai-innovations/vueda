74b1b9bf4ea06d3e7c1fceef0d59e85645bede19
Jayden Kneller <jayden@arrai.com>
2024-03-21 11:56:36 -0600

    fix: Change info router, so we use app_label and model instead of pk.

--- server/vueda/info/viewsets.py
+++ server/vueda/info/viewsets.py
@@ -41,11 +41,10 @@ class ModelInfoViewSet(FlexFieldsMixin, generics.ListAPIView, generics.RetrieveA
         return ContentType.objects.all().filter(pk__in=get_registered_content_types())
 
     # noinspection PyMethodOverriding
-    def get_object(self, pk):
-        return generics.get_object_or_404(ContentType, pk=pk)
+    def get_object(self, app_label, model):
+        return generics.get_object_or_404(ContentType, app_label=app_label, model=model.replace("_", ""))
 
     def retrieve(self, request, *args, **kwargs):
-        pk = kwargs.pop("pk")
-        instance = self.get_object(pk)
+        instance = self.get_object(kwargs["app_label"], kwargs["model"])
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
