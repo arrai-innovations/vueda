04c0c3f8d653177f0ce6a7604ec8bf636dd285bc
Jayden Kneller <jayden@arrai.com>
2024-03-21 11:59:58 -0600

    fix: Add code to get model fields, expands, and filtering.

--- server/vueda/info/serializers.py
+++ server/vueda/info/serializers.py
@@ -59,15 +59,31 @@ class ModelInfoSerializer(FlexFieldsSerializerMixin, serializers.ModelSerializer
         fields = []
         for field_name, field in serializer().get_fields().items():
             many = isinstance(field, serializers.ListField)
-            fields.append(
-                {
-                    "name": field_name,
-                    "type": field.child.__class__.__name__ if many else field.__class__.__name__,
-                    "many": many,
-                    "read_only": field.read_only,
-                    "required": field.required,
-                }
-            )
+            field_data = {
+                "name": field_name,
+                "label": field.label,
+                "type": field.child.__class__.__name__ if many else field.__class__.__name__,
+                "many": many,
+                "read_only": field.read_only,
+                "required": field.required,
+            }
+            if field.help_text is not None:
+                field_data["help_text"] = field.help_text
+            if hasattr(field, "max_value") and field.max_value:
+                field_data["max_value"] = field.max_value
+            if hasattr(field, "min_value") and field.min_value:
+                field_data["min_value"] = field.min_value
+            if hasattr(field, "max_length") and field.max_length:
+                field_data["max_length"] = field.max_length
+            if hasattr(field, "min_length") and field.min_length:
+                field_data["min_length"] = field.min_length
+            if hasattr(field, "max_digits") and field.max_digits:
+                field_data["max_digits"] = field.max_digits
+            if hasattr(field, "decimal_places") and field.decimal_places:
+                field_data["decimal_places"] = field.decimal_places
+            if hasattr(field, "choices") and field.choices:
+                field_data["choices"] = field.choices
+            fields.append(field_data)
         return fields
 
     def get_model_actions(self, instance):
@@ -89,14 +105,15 @@ class ModelInfoSerializer(FlexFieldsSerializerMixin, serializers.ModelSerializer
         Get the expands for a model and their own metadata.
         """
         # Similar to actions, we'll need to have a canonical serializer to determine what expands are available
-        # todo: this is a placeholder
-        return [
-            {
-                "name": expand,
-                "description": f"Expand {expand}",
-            }
-            for expand in ["expand1", "expand2"]
-        ]
+        serializer = self.canonical["serializer"]  # type: serializers.ModelSerializer
+
+        if hasattr(serializer.Meta, "expandable_fields"):
+            return [
+                {"name": expand, "description": f"Expand {expand}", "fields": expand_data[1]["fields"]}
+                for expand, expand_data in serializer.Meta.expandable_fields.items()
+            ]
+
+        return []
 
     def get_model_ordering(self, instance):
         """
@@ -117,11 +134,10 @@ class ModelInfoSerializer(FlexFieldsSerializerMixin, serializers.ModelSerializer
         Get the filtering fields for a model and their own metadata.
         """
         # Similar to actions, we'll need to have a canonical viewset to determine what fields are available
-        # todo: this is a placeholder
-        return [
-            {
-                "name": a_filter,
-                "type": "filter_type",
-            }
-            for a_filter in ["filter1", "filter2"]
-        ]
+        viewset = self.canonical["viewset"]  # type: viewsets.VuedaViewSet
+
+        if hasattr(viewset, "filterset_class"):
+            filterset = viewset.filterset_class
+            return filterset.Meta.fields.copy()
+
+        return []
