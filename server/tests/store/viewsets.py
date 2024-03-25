bd0afcc5bf1930ed9acf0ad866e06a3700ac5a80
Jayden Kneller <jayden@arrai.com>
2024-03-25 14:13:51 -0600

    feat: Add history, permissions, and actions to some store models.

--- server/tests/store/viewsets.py
+++ server/tests/store/viewsets.py
@@ -1,24 +1,34 @@
+from dateutil.relativedelta import relativedelta
+from django.http import Http404
+from django.utils.timezone import now
+from rest_framework.decorators import action
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+
 import tests.store.filtersets as my_filtersets
 import tests.store.models as my_models
 import tests.store.serializers as my_serializers
+from tests.permissions import IsAdminUser
+from tests.permissions import IsCartOrOrderCreator
 from vueda.core.permissions import ObjectPermissions
+from vueda.core.viewsets import VuedaHistoryViewSet
 from vueda.core.viewsets import VuedaViewSet
 
 
-class CustomerViewSet(VuedaViewSet):
+class CustomerViewSet(VuedaHistoryViewSet):
     queryset = my_models.Customer.objects.all()
     serializer_class = my_serializers.CustomerSerializer
     permission_classes = [ObjectPermissions]
 
 
-class DistributorViewSet(VuedaViewSet):
+class DistributorViewSet(VuedaHistoryViewSet):
     queryset = my_models.Distributor.objects.all()
     serializer_class = my_serializers.DistributorSerializer
     permission_classes = [ObjectPermissions]
     filterset_class = my_filtersets.DistributorFilterSet
 
 
-class ProductViewSet(VuedaViewSet):
+class ProductViewSet(VuedaHistoryViewSet):
     queryset = my_models.Product.objects.all()
     serializer_class = my_serializers.ProductSerializer
     permission_classes = [ObjectPermissions]
@@ -31,7 +41,7 @@ class OptionTypeViewSet(VuedaViewSet):
     permission_classes = [ObjectPermissions]
 
 
-class ProductOptionViewSet(VuedaViewSet):
+class ProductOptionViewSet(VuedaHistoryViewSet):
     queryset = my_models.ProductOption.objects.all()
     serializer_class = my_serializers.ProductOptionSerializer
     permission_classes = [ObjectPermissions]
@@ -41,7 +51,39 @@ class ProductOptionViewSet(VuedaViewSet):
 class CartViewSet(VuedaViewSet):
     queryset = my_models.Cart.objects.all()
     serializer_class = my_serializers.CartSerializer
-    permission_classes = [ObjectPermissions]
+    permission_classes = [ObjectPermissions & (IsCartOrOrderCreator | IsAdminUser)]
+    permitted_expands = ["cart_items", "customer"]
+
+    @action(detail=True, methods=["put", "patch"], permission_classes=(IsCartOrOrderCreator,))
+    def create_order(self, request, pk):
+        cart = self.queryset.filter(pk=pk).first()
+        if cart is None:
+            raise Http404
+
+        user = request.user
+        if cart.customer.user != user:
+            raise PermissionDenied
+
+        customer_order = my_models.CustomerOrder.objects.create(
+            customer=cart.customer,
+            order_number=my_models.CustomerOrder.get_next_order_number(),
+            order_state=my_models.OrderState.objects.get(code="new"),
+        )
+        for cart_item in cart.cart_items.all():
+            my_models.OrderItem.objects.create(
+                customer_order=customer_order,
+                product_option=cart_item.product_option,
+                quantity=cart_item.quantity,
+            )
+        return Response(customer_order.pk)
+
+    @action(detail=False, methods=["get"], permission_classes=(IsAdminUser,))
+    def abandoned_carts_count(self, request):
+        if not request.user.has_perm("store_customer_read"):
+            raise PermissionDenied
+
+        abandoned_carts = self.queryset.filter(last_modified__gt=now() - relativedelta(days=7))
+        return Response(abandoned_carts.count())
 
 
 class CartItemViewSet(VuedaViewSet):
@@ -50,7 +92,7 @@ class CartItemViewSet(VuedaViewSet):
     permission_classes = [ObjectPermissions]
 
 
-class CustomerOrderViewSet(VuedaViewSet):
+class CustomerOrderViewSet(VuedaHistoryViewSet):
     queryset = my_models.CustomerOrder.objects.all()
     serializer_class = my_serializers.CustomerOrderSerializer
     permission_classes = [ObjectPermissions]
