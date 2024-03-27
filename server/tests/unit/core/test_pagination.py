dfa314b9d3661dd189c9c7b7977ed59c0f39f863
Jayden Kneller <jayden@arrai.com>
2024-03-27 11:31:00 -0600

    test: Adjust tests to use a context manager for the rest framework settings, and use settings for the other customizations.

--- server/tests/unit/core/test_pagination.py
+++ server/tests/unit/core/test_pagination.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 
 from tests.conftest import BaseTestCommonModelViewSet
 from tests.models import Product
+from tests.utils import adjust_page_size
 from vueda.core.pagination import VUEDAPageNumberPagination
 
 
@@ -54,26 +55,18 @@ class TestPagination(BaseTestCommonModelViewSet):
         api_client.force_authenticate(user=user)
         return api_client
 
-    def test_get_paginated_response(self, authenticated_client, page_data):
-        # because rest framework loads settings on class import there's no way to override through 'settings'
-        VUEDAPageNumberPagination.page_size = 5
-        VUEDAPageNumberPagination.max_page_size = 200
-        VUEDAPageNumberPagination.page_query_param = "p"
-        VUEDAPageNumberPagination.page_size_query_param = "ps"
-
-        url = reverse("tests.product-list")
-        response = authenticated_client.get(url, data={"ps": "5"}, format="json")
-        response_data = {x: y for x, y in response.data.items() if x != "results"}
-        assert response.status_code == 200
-        assert response_data["perPage"] == 5
-        assert response_data["totalPages"] == 3
-        assert response_data["totalRecords"] == len(self.page_data_arguments)
+    def test_get_paginated_response(self, settings, authenticated_client, page_data):
+        with adjust_page_size(settings, 5):
+            url = reverse("tests.product-list")
+            response = authenticated_client.get(url, format="json")
+            response_data = {x: y for x, y in response.data.items() if x != "results"}
+            assert response.status_code == 200
+            assert response_data["perPage"] == 5
+            assert response_data["totalPages"] == 3
+            assert response_data["totalRecords"] == len(self.page_data_arguments)
 
     def test_page_size_query_param(self, settings, authenticated_client, page_data):
         settings.PAGE_SIZE_QUERY_PARAM = "our_ps"
-        VUEDAPageNumberPagination.page_size = 100
-        VUEDAPageNumberPagination.max_page_size = 200
-        VUEDAPageNumberPagination.page_query_param = "p"
 
         # Store the original function, so we can call it in the mocked function.
         orig_get_page_size = VUEDAPageNumberPagination.get_page_size
@@ -84,7 +77,6 @@ class TestPagination(BaseTestCommonModelViewSet):
             return page_size
 
         with patch.object(VUEDAPageNumberPagination, "get_page_size", get_page_size) as mocked_get_page_size:
-            VUEDAPageNumberPagination.page_size_query_param = "our_ps"
             url = reverse("tests.product-list")
             authenticated_client.get(url, data={"our_ps": "151"}, format="json")
 
@@ -92,25 +84,19 @@ class TestPagination(BaseTestCommonModelViewSet):
 
     def test_page_query_param(self, settings, authenticated_client, page_data):
         settings.PAGE_QUERY_PARAM = "our_p"
-        VUEDAPageNumberPagination.page_size = 5
-        VUEDAPageNumberPagination.max_page_size = 200
-        VUEDAPageNumberPagination.page_query_param = "our_p"
-        VUEDAPageNumberPagination.page_size_query_param = "ps"
 
-        url = reverse("tests.product-list")
-        response = authenticated_client.get(url, data={"ps": "5", "our_p": 3}, format="json")
-        response_data = {x: y for x, y in response.data.items() if x != "results"}
-        assert response.status_code == 200
-        assert response_data["perPage"] == 5
-        assert response_data["totalPages"] == 3
-        assert len(response.data["results"]) == 3
-        assert response_data["totalRecords"] == len(self.page_data_arguments)
+        with adjust_page_size(settings, 5):
+            url = reverse("tests.product-list")
+            response = authenticated_client.get(url, data={"our_p": 3}, format="json")
+            response_data = {x: y for x, y in response.data.items() if x != "results"}
+            assert response.status_code == 200
+            assert response_data["perPage"] == 5
+            assert response_data["totalPages"] == 3
+            assert len(response.data["results"]) == 3
+            assert response_data["totalRecords"] == len(self.page_data_arguments)
 
-    def test_max_page_size(self, authenticated_client, page_data):
-        VUEDAPageNumberPagination.max_page_size = 99
-        VUEDAPageNumberPagination.page_size = 100
-        VUEDAPageNumberPagination.page_query_param = "p"
-        VUEDAPageNumberPagination.page_size_query_param = "ps"
+    def test_max_page_size(self, settings, authenticated_client, page_data):
+        settings.MAX_PAGE_SIZE = 99
 
         # Store the original function, so we can call it in the mocked function.
         orig_get_page_size = VUEDAPageNumberPagination.get_page_size
@@ -122,6 +108,6 @@ class TestPagination(BaseTestCommonModelViewSet):
 
         with patch.object(VUEDAPageNumberPagination, "get_page_size", get_page_size) as mocked_get_page_size:
             url = reverse("tests.product-list")
-            authenticated_client.get(url, data={"ps": "100"}, format="json")
+            authenticated_client.get(url, format="json")
 
             assert mocked_get_page_size._returned_page_size == 99
