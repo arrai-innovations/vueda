81b8a4b2c8a1973c4b7e22d6d19dd893ce4478e7
Jayden Kneller <jayden@arrai.com>
2024-04-03 14:52:02 -0600

    fix: Guard login urls with debug mode.

--- server/vueda/user/urls.py
+++ server/vueda/user/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth import views
 from django.urls import path
 
@@ -6,6 +7,10 @@ from vueda.user.views import WhoIsView
 
 urlpatterns = [
     path("who-is/", WhoIsView.as_view(), name="who-is"),
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("login/", views.LoginView.as_view(), name="login"),
+        path("logout/", views.LogoutView.as_view(), name="logout"),
+    ]
