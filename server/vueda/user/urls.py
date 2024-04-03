6c027b04eb45811baded2993e3dfbfa7a661d182
Jayden Kneller <jayden@arrai.com>
2024-04-03 14:38:04 -0600

    feat: Use the django logout view, with a trimmed down template.

--- server/vueda/user/urls.py
+++ server/vueda/user/urls.py
@@ -7,4 +7,5 @@ from vueda.user.views import WhoIsView
 urlpatterns = [
     path("who-is/", WhoIsView.as_view(), name="who-is"),
     path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
