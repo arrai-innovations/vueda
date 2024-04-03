c441ea50180e7730b826d46f04b5fb6e09503d5f
Jayden Kneller <jayden@arrai.com>
2024-04-03 14:33:58 -0600

    feat: Use the django login view, with a trimmed down template.

--- server/vueda/user/urls.py
+++ server/vueda/user/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views
 from django.urls import path
 
 from vueda.user.views import WhoIsView
@@ -5,4 +6,5 @@ from vueda.user.views import WhoIsView
 
 urlpatterns = [
     path("who-is/", WhoIsView.as_view(), name="who-is"),
+    path("login/", views.LoginView.as_view(), name="login"),
 ]
