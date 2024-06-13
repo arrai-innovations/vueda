9c52ac5682d7782ef194bf6ac498bf277ef1daa8
Jayden Kneller <jayden@arrai.com>
2024-06-13 16:20:10 -0600

    fix: Move permission into initial migration and remove manual creation.

--- server/tests/store/migrations/0001_initial.py
+++ server/tests/store/migrations/0001_initial.py
@@ -83,6 +83,7 @@ class Migration(migrations.Migration):
             ],
             options={
                 "default_permissions": ("create", "read", "update", "delete", "list"),
+                "permissions": [("fulfill_orders", "Can fulfill orders")],
             },
         ),
         migrations.CreateModel(
