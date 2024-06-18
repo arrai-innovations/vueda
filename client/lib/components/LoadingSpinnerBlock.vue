97cfc51f081ded0421a6195181f6dc4ae7e1515e
Joel Hillacre <joel@403forbidden.ca>
2024-06-18 14:36:43 -0600

    refactor: use seperate loading spinners for inline or block context, allow overriding of both.

---
+++ client/lib/components/LoadingSpinnerBlock.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ProgressSpinner from "primevue/progressspinner";
+import { inject } from "vue";
+
+// Try to inject the provided component, or use the default
+const loadingSpinnerComponent = inject("vuedaLoadingSpinnerBlock", null);
+</script>
+
+<template>
+    <component :is="loadingSpinnerComponent" v-if="loadingSpinnerComponent" />
+    <ProgressSpinner
+        v-else
+        :pt="{
+            root: {
+                class: ['w-full', 'h-full', 'relative', 'mx-1'],
+            },
+        }"
+    />
+</template>
---
+++ client/lib/components/LoadingSpinnerInline.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ProgressSpinner from "primevue/progressspinner";
+import { inject } from "vue";
+
+// Try to inject the provided component, or use the default
+const loadingSpinnerComponent = inject("vuedaLoadingSpinnerInline", null);
+</script>
+
+<template>
+    <component :is="loadingSpinnerComponent" v-if="loadingSpinnerComponent" />
+    <ProgressSpinner
+        v-else
+        :pt="{
+            root: {
+                class: ['w-4', 'h-4', 'inline-block', 'relative', 'mx-1'],
+            },
+        }"
+    />
+</template>
