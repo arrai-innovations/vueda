23ad65a449da70f1b4daa6976f94e85a75c1c231
Joel Hillacre <joel@403forbidden.ca>
2024-06-18 14:40:38 -0600

    chore: remove log.

--- client/lib/utils/defaultTheme.js
+++ client/lib/utils/defaultTheme.js
@@ -92,5 +92,4 @@ export default function () {
             themeStore.registerVariant(componentName, variantName, defaultVariants[componentName][variantName]);
         }
     }
-    console.log(themeStore);
 }
