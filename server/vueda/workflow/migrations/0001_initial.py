83ddf81ad7b850e316273d076ca96bbf48d9ad2c
Jayden Kneller <jayden@arrai.com>
2024-07-05 14:30:34 -0600

    fix: Add sites dependency, so if you migrate workflow alone, it doesn't leave the site broken.

--- server/vueda/workflow/migrations/0001_initial.py
+++ server/vueda/workflow/migrations/0001_initial.py
@@ -25,6 +25,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ("sites", "0002_alter_domain_unique"),
     ]
 
     operations = [
