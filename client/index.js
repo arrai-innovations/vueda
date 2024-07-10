06dac7b2ae25f2c697b2708adc9226430cb832c9
Joel Hillacre <joel@403forbidden.ca>
2024-07-10 10:31:03 -0600

    chore: packaging.

---
+++ client/index.js
@@ -0,0 +1,4 @@
+throw new Error(
+    "Don't import from the package, use paths to the files you need and the '@vueda' alias.\n" +
+        "For example. `import { useForm } from '@vueda/use/useForm/js'`.",
+);
