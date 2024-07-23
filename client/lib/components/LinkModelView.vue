cc67abb69727d40f8768301e9076adc584133796
Joel Hillacre <joel@403forbidden.ca>
2024-07-23 13:49:55 -0600

    fix: handle bad routes in userland, not thrown errors/warnings.

--- client/lib/components/LinkModelView.vue
+++ client/lib/components/LinkModelView.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { getCRUDForTo, isDetailView } from "@vueda/router/getCrud.js";
+import omit from "lodash-es/omit.js";
 import Button from "primevue/button";
 import { computed } from "vue";
+import { useRouter } from "vue-router";
 
-// import { useRouter } from "vue-router";
 // import isFunction from "lodash-es/isFunction.js";
 
 defineOptions({ inheritAttrs: false });
@@ -24,6 +25,10 @@ const props = defineProps({
     button: {
         type: Boolean,
         default: false,
+    },
+    buttonClass: {
+        type: String,
+        default: undefined,
     },
     view: {
         type: [String, Array, Object],
@@ -67,8 +72,9 @@ const viewToUse = computed(() => {
 // const result = computed(() => permissionsPerView.value[viewToUse.value]);
 const isDetailViewComputed = computed(() => isDetailView(viewToUse.value));
 
+const pkValid = computed(() => (isDetailViewComputed.value && props.pk) || !isDetailViewComputed.value);
 const toRouteArgs = computed(() => {
-    return viewToUse.value
+    return pkValid.value && viewToUse.value
         ? getCRUDForTo({
               app: props.app,
               model: props.model,
@@ -77,13 +83,19 @@ const toRouteArgs = computed(() => {
           })
         : undefined;
 });
-const actionDisabled = computed(() => {
-    return isDetailViewComputed.value && (!props.pk || (Array.isArray(props.pk) && props.pk.length === 0));
+const actionDisabled = computed(
+    () => isDetailViewComputed.value && (!props.pk || (Array.isArray(props.pk) && props.pk.length === 0)),
+);
+const router = useRouter();
+const toRoute = computed(() =>
+    router.hasRoute(toRouteArgs.value?.name) ? router.resolve(toRouteArgs.value) : undefined,
+);
+const href = computed(() => {
+    return toRoute.value.href;
 });
-// const router = useRouter();
-// const toRoute = computed(() => {
-//     return router.resolve(toRouteArgs.value);
-// });
+const navigate = () => {
+    router.push(toRoute.value);
+};
 // const toRouteProps = computed(() => {
 //     const props = toRoute.value.matched?.[0]?.props?.default;
 //     if (isFunction(props)) {
@@ -94,33 +106,31 @@ const actionDisabled = computed(() => {
 </script>
 
 <template>
-    <div>
-        <router-link v-if="(isDetailViewComputed && pk) || !isDetailViewComputed" custom :to="toRouteArgs">
-            <template #default="slotProps">
-                <slot
-                    v-if="$slots.button"
-                    :href="button ? undefined : slotProps.href"
-                    :label="label"
-                    :link="!button"
-                    name="button"
-                    :navigate="slotProps.navigate"
-                    v-bind="$attrs"
-                />
-                <Button
-                    v-else
-                    v-bind="$attrs"
-                    :disabled="actionDisabled"
-                    :href="button ? undefined : slotProps.href"
-                    :label="label"
-                    :link="!button"
-                    @click="slotProps.navigate"
-                >
-                    <template v-for="(_, slot) in $slots" #[slot]="slotProps">
-                        <slot :name="slot" v-bind="slotProps || {}" />
-                    </template>
-                </Button>
+    <div :class="$attrs.class">
+        <slot
+            v-if="$slots.button && toRoute"
+            :class="buttonClass"
+            :href="button ? undefined : href"
+            :label="label"
+            :link="!button"
+            name="button"
+            :navigate="navigate"
+            v-bind="omit($attrs, ['class'])"
+        />
+        <Button
+            v-else-if="toRoute"
+            v-bind="omit($attrs, ['class'])"
+            :class="buttonClass"
+            :disabled="actionDisabled"
+            :href="button ? undefined : href"
+            :label="label"
+            :link="!button"
+            @click="navigate"
+        >
+            <template v-for="(_, slot) in $slots" #[slot]="slotProps">
+                <slot :name="slot" v-bind="slotProps || {}" />
             </template>
-        </router-link>
-        <slot v-else></slot>
+        </Button>
+        <slot v-else name="invalid-route"> Invalid route or permission denied. </slot>
     </div>
 </template>
