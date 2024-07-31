51b4308dd8c2f395ce1cf30673c628a388335764
Zoey Zhou <zoey@arrai.com>
2024-07-31 10:51:05 -0600

    chore: fields data return name

--- server/vueda/info/serializers.py
+++ server/vueda/info/serializers.py
@@ -218,6 +218,7 @@ class ModelInfoSerializer(FlexFieldsSerializerMixin, serializers.ModelSerializer
                 "label": effective_label,
                 "type": field_type,
                 "many": many,
+                "name": field_name,
                 "read_only": field.read_only,
                 "required": field.required,
             }
