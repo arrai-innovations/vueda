ba94cb960f4f4cda52732960a7c285dca1ba83a2
Jayden Kneller <jayden@arrai.com>
2024-08-29 15:44:30 -0600

    remove: Remove open api customizations for now.

--- server/vueda/info/serializers.py
+++ server/vueda/info/serializers.py
@@ -16,7 +16,6 @@ from rest_flex_fields.serializers import FlexFieldsSerializerMixin
 from rest_framework import serializers  # noqa F401
 from rest_framework import viewsets  # noqa F401
 
-from vueda.core import open_api
 from vueda.core.serializers import VuedaExpandableFieldsSerializerMixin
 from vueda.info.registration import get_registration
 
@@ -690,21 +689,6 @@ class ModelInfoSerializer(VuedaExpandableFieldsSerializerMixin, FlexFieldsSerial
                     filtering_data[filter_obj.field_name].update(extra_data)
 
         return filtering_data
-
-
-# This adds the expands as default, so we can get back the data and
-class OpenAPIModelInfoSerializer(ModelInfoSerializer):
-    def get_fields(self):
-        fields = super().get_fields()
-
-        for field_name in sorted(ModelInfoSerializer.Meta.expandable_fields):
-            serializer_name = "".join([part.capitalize() for part in field_name.split("_")])
-            serializer = getattr(open_api, serializer_name)
-            if serializer is None:
-                raise RuntimeError(f"Unable to find a serializer named '{serializer_name}' in open_api.py.")
-            fields[field_name] = serializer(many=True, required=False)
-
-        return fields
 
 
 class ModelInfoChoicesSerializer(
