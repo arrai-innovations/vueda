c176a393f3d31096261c9b89ca074711af611927
Joel Hillacre <joel@403forbidden.ca>
2024-11-06 11:46:10 -0700

    chore: sort widget theme entries.

--- client/lib/theme/vueda-tailwind/widgets/index.js
+++ client/lib/theme/vueda-tailwind/widgets/index.js
@@ -10,51 +10,23 @@ export default {
             class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
         },
     },
-    WidgetGenericAutoComplete: {
-        root: {
-            class: [],
-        },
-        inner: {
-            class: ["flex flex-row gap-2 flex-wrap [&_input]:w-full"],
-        },
-        dropdownOuter: {
-            class: ["flex flex-col flex-shrink"],
-        },
-        autoCompleteOuter: {
-            class: ["flex flex-col flex-grow"],
-        },
-        label: {
-            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
-        },
-    },
-    WidgetMultiSelect: {
-        root: {
-            class: [],
-        },
-        inner: {
-            class: ["flex flex-col"],
-        },
-        label: {
-            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
-        },
-    },
-    WidgetSearchableSelect: {
-        root: {
-            class: [],
-        },
-        inner: {
-            class: ["flex flex-col gap-2 flex-wrap [&_input]:w-full"],
-        },
-        label: {
-            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
-        },
-    },
     WidgetCheckbox: {
         root: {
             class: ["ml-3"],
         },
         inner: {
             class: ["flex"],
+        },
+        label: {
+            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
+        },
+    },
+    WidgetDatePicker: {
+        root: {
+            class: [],
+        },
+        inner: {
+            class: ["flex flex-col"],
         },
         label: {
             class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
@@ -72,45 +44,6 @@ export default {
         },
         innerItem: {
             class: ["flex flex-col flex-grow"],
-        },
-    },
-    WidgetDatePicker: {
-        root: {
-            class: [],
-        },
-        inner: {
-            class: ["flex flex-col"],
-        },
-        label: {
-            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
-        },
-    },
-    WidgetHtml: {
-        root: {
-            class: [],
-        },
-        inner: {
-            class: ["flex flex-col"],
-        },
-        label: {
-            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
-        },
-        menu: {
-            class: [],
-        },
-        editor: {
-            class: [],
-        },
-    },
-    WidgetInput: {
-        root: {
-            class: [],
-        },
-        inner: {
-            class: ["flex flex-col gap-2 flex-wrap [&_input]:w-full"],
-        },
-        label: {
-            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
         },
     },
     WidgetFile: {
@@ -133,6 +66,40 @@ export default {
             class: ["flex gap-4"],
         },
     },
+    WidgetGenericAutoComplete: {
+        root: {
+            class: [],
+        },
+        inner: {
+            class: ["flex flex-row gap-2 flex-wrap [&_input]:w-full"],
+        },
+        dropdownOuter: {
+            class: ["flex flex-col flex-shrink"],
+        },
+        autoCompleteOuter: {
+            class: ["flex flex-col flex-grow"],
+        },
+        label: {
+            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
+        },
+    },
+    WidgetHtml: {
+        root: {
+            class: [],
+        },
+        inner: {
+            class: ["flex flex-col"],
+        },
+        label: {
+            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
+        },
+        menu: {
+            class: [],
+        },
+        editor: {
+            class: [],
+        },
+    },
     WidgetImage: {
         root: {
             class: [],
@@ -142,6 +109,28 @@ export default {
         },
         image: {
             class: ["flex flex-wrap justify-between"],
+        },
+        label: {
+            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
+        },
+    },
+    WidgetInput: {
+        root: {
+            class: [],
+        },
+        inner: {
+            class: ["flex flex-col gap-2 flex-wrap [&_input]:w-full"],
+        },
+        label: {
+            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
+        },
+    },
+    WidgetMultiSelect: {
+        root: {
+            class: [],
+        },
+        inner: {
+            class: ["flex flex-col"],
         },
         label: {
             class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
@@ -175,6 +164,17 @@ export default {
         },
         input: {
             class: ["ml-3"],
+        },
+    },
+    WidgetSearchableSelect: {
+        root: {
+            class: [],
+        },
+        inner: {
+            class: ["flex flex-col gap-2 flex-wrap [&_input]:w-full"],
+        },
+        label: {
+            class: ["ml-3 leading-7", "text-surface-900/60 dark:text-white/60"],
         },
     },
     WidgetSelect: {
