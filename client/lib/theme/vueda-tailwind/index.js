9744be5407d99b066cc0b01af360e4f823516aa3
Joel Hillacre <joel@403forbidden.ca>
2024-12-04 09:11:14 -0700

    feat: add StickyBar component for scroll-based visibility

--- client/lib/theme/vueda-tailwind/index.js
+++ client/lib/theme/vueda-tailwind/index.js
@@ -18,4 +18,27 @@ export default {
             class: [],
         },
     },
+    StickyBar: {
+        root: (args) => {
+            return {
+                class: {
+                    "sticky top-[-1px] z-30": true,
+                    "transition-transform duration-300 ease-in-out transform": true,
+                    "translate-y-[-100%]": args?.hidden,
+                    "translate-y-0": !args?.hidden,
+                },
+            };
+        },
+        inner: {
+            //bg-zinc-100 dark:bg-zinc-800 pt-[2px]
+            class: ["bg-white dark:bg-black", "pt-2"],
+        },
+        gradient: {
+            class: [
+                //w-full h-1 md:h-2 2xl:h-4 bg-gradient-to-b from-zinc-100 to-transparent dark:from-zinc-800 dark:to-transparent
+                "w-full h-1 md:h-2 2xl:h-4",
+                "bg-gradient-to-b from-white to-transparent dark:from-black dark:to-transparent",
+            ],
+        },
+    },
 };
