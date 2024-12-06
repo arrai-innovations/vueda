06e412a2b95c18e1ab53b7afe89a5779982dd070
Joel Hillacre <joel@403forbidden.ca>
2024-12-06 16:17:52 -0700

    fix: duplicate class.

--- client/lib/views/ViewUpdate.vue
+++ client/lib/views/ViewUpdate.vue
@@ -299,10 +299,7 @@ useWarnings(toRef(props, "app"), toRef(props, "model"), formContext, viewName, t
             </template>
         </page-title>
         <sticky-bar class="w-full">
-            <div
-                class="flex flex-wrap gap-1 2xl:gap-2 w-full w-full sm:w-fit sm:max-w-max"
-                data-qa="update-action-buttons"
-            >
+            <div class="flex flex-wrap gap-1 2xl:gap-2 w-full sm:w-fit sm:max-w-max" data-qa="update-action-buttons">
                 <slot
                     :form="formId"
                     label="Submit"
