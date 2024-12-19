88c6041ca16d0219fb462b10efbdd09ff7c08544
Zoey Zhou <zoey@arrai.com>
2024-12-19 14:37:47 -0700

    chore: restrict server info to only GET

--- server/vueda/info/views.py
+++ server/vueda/info/views.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
 from vueda import __version__ as server_version
 
 
+@api_view(["GET"])
 def server_info_view(request):
     return JsonResponse(
         {
