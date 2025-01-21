e9b31ac606c03160823dba19be1d479f4af36aee
Jayden Kneller <jayden@arrai.com>
2025-01-21 13:51:00 -0700

    refactor: Make some changes to VuedaValidationError, to allow filtering warnings and move the complexity into the recursive functions.

--- server/vueda/core/exceptions.py
+++ server/vueda/core/exceptions.py
@@ -75,22 +75,38 @@ def page_not_found(request, exception, *args, **kwargs):
     return JsonResponse({"error": "Not Found (404)"}, status=HTTP_404_NOT_FOUND)
 
 
-def _get_error_details(data, default_code=None):
+def _get_error_details(data, default_code=None, is_warning=False):
     """
     Descend into a nested data structure, forcing any
     lazy translation strings or strings into `ErrorDetail`.
     """
     if isinstance(data, VuedaValidationError):
-        return data
-    elif isinstance(data, (list, tuple)):
-        ret = [_get_error_details(item, default_code) for item in data]
+        detail = data.detail
+        # Don't double up lists
+        if not isinstance(detail, list):
+            return force_str([detail])
+        return force_str(detail)
+
+    if is_warning:
+        data = get_error_details_as_warning(data)
+
+    if isinstance(data, (list, tuple)):
+        ret = []
+        for item in data:
+            if isinstance(item, VuedaValidationError):
+                ret.append(_get_error_details(item, item.code, item.is_warning))
+            else:
+                ret.append(_get_error_details(item, default_code, is_warning))
         if isinstance(data, ReturnList):
             return ReturnList(ret, serializer=data.serializer)
+
         return ret
+
     elif isinstance(data, dict):
         ret = {key: _get_error_details(value, default_code) for key, value in data.items()}
         if isinstance(data, ReturnDict):
             return ReturnDict(ret, serializer=data.serializer)
+
         return ret
 
     text = force_str(data)
@@ -110,7 +126,11 @@ def get_error_details_as_warning(detail):
             details[key] = errors
         return details
     else:
-        return {"warnings": detail}
+        warnings = _get_error_details(detail, default_code="warning")
+        # If the value is a list, tuple or dict, we don't want another list.
+        if not isinstance(warnings, (list, tuple, dict)):
+            return {"warnings": [warnings]}
+        return {"warnings": warnings}
 
 
 class VuedaValidationError(ValidationError):
@@ -119,20 +139,19 @@ class VuedaValidationError(ValidationError):
     def __init__(self, detail=None, code=None, is_warning=False):
         if detail is None:
             detail = self.default_detail
-        if code is None:
-            code = self.default_code
 
-        # For validation failures, we may collect many errors together,
-        # so the details should always be coerced to a list if not already.
-        if isinstance(detail, tuple):
-            detail = list(detail)
-        elif not isinstance(detail, dict) and not isinstance(detail, list):
-            detail = [detail]
-        if is_warning:
-            if isinstance(detail, dict):
-                error_detail = get_error_details_as_warning(detail)
-                self.detail = _get_error_details(error_detail, code)
+        if code is None:
+            if is_warning:
+                code = "warning"
             else:
-                self.detail = [_get_error_details({"warnings": detail}, code)]
+                code = self.default_code
+
+        self.code = code
+        self.is_warning = is_warning
+        error_details = _get_error_details(detail, code, is_warning)
+
+        # If the value is a list, tuple or dict, we don't want another list.
+        if not isinstance(error_details, (list, tuple, dict)):
+            self.detail = [error_details]
         else:
-            self.detail = _get_error_details(detail, code)
+            self.detail = error_details
