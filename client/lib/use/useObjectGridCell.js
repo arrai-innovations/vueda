4be54aebfaf004c30a0a415c2fca52e9ce597b1a
Joel Hillacre <joel@403forbidden.ca>
2025-02-20 10:14:19 -0700

    fix(use-object-grid-cell): ensure formatted falls back to field.value before name

--- client/lib/use/useObjectGridCell.js
+++ client/lib/use/useObjectGridCell.js
@@ -3,7 +3,12 @@ import { computed } from "vue";
 
 export function useObjectGridCell(props) {
     const formattedComputed = computed(() =>
-        unifiedGet(props.obj, props.relatedObject, props.calculatedObject, props.field.formatted ?? props.field.name),
+        unifiedGet(
+            props.obj,
+            props.relatedObject,
+            props.calculatedObject,
+            props.field.formatted ?? props.field.value ?? props.field.name,
+        ),
     );
     const valueComputed = computed(() =>
         unifiedGet(props.obj, props.relatedObject, props.calculatedObject, props.field.value ?? props.field.name),
