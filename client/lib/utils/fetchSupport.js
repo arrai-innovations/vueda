1645f0ffa51f522883f07f72dc655410ba254be6
Joel Hillacre <joel@403forbidden.ca>
2025-02-25 09:49:19 -0700

    feat(fetch-support): add cncellable promises for fetch requests.

--- client/lib/utils/fetchSupport.js
+++ client/lib/utils/fetchSupport.js
@@ -21,6 +21,20 @@ export async function getJsonOrText(response) {
 }
 
 /**
+ * A Promise that can be cancelled.
+ *
+ * @typedef {Promise<any> & { cancel: () => Promise<void>|void }} CancellablePromise
+ * future: webstorm doesn't understand import("@arrai-innovations/reactive-helpers/use/cancellableIntent.js").CancellablePromise
+ *  so we are copying the type definition here
+ */
+
+/**
+ * A Promise that might be able to be cancelled.
+ *
+ * @typedef {Promise<any> & { cancel?: () => Promise<void>|void }} MaybeCancellablePromise
+ */
+
+/**
  * Fetches a URL and returns the response.
  *
  * @param {string} url - The URL to fetch.
@@ -29,10 +43,10 @@ export async function getJsonOrText(response) {
  * @param {import("@vueda/utils/errors.js").FetchError} [errorClass=FetchError] - The error class to throw.
  * @param {Set<number>} [emptyResponseCodes=new Set([204])] - The response codes that are considered empty.
  * @param {*} [emptyResponseValue=undefined] - The value to return if the response is empty.
- * @returns {Promise<{[key: string]: any}|string|[emptyResponseValue: any]>} The response data.
+ * @returns {CancellablePromise<{[key: string]: any}|string|[emptyResponseValue: any]>} The response data.
  * @throws {import("@vueda/utils/errors.js").FetchError} If the fetch fails, in a way that is not an empty response.
  */
-export const fetchHelper = async (
+export const fetchHelper = (
     url,
     options = {},
     messagePrefix,
@@ -45,22 +59,37 @@ export const fetchHelper = async (
         "X-CSRFToken": getCSRFValue(),
     };
     const headers = { ...defaultHeaders, ...options.headers };
-    let response;
-    try {
-        response = await fetch(url, {
+    const controller = new AbortController();
+
+    const promise = new Promise((resolve, reject) => {
+        fetch(url, {
             ...options,
             headers,
             credentials: "include",
-        });
-    } catch (error) {
-        throw new errorClass(messagePrefix, error, {});
-    }
-    const responseData = await getJsonOrText(response);
-    if (!response.ok) {
-        if (emptyResponseCodes.has(response.status) && emptyResponseValue !== undefined) {
-            return emptyResponseValue;
-        }
-        throw new errorClass(messagePrefix, response, responseData);
-    }
-    return responseData;
+            signal: controller.signal,
+        })
+            .then(async (response) => {
+                const responseData = await getJsonOrText(response);
+                if (!response.ok) {
+                    if (emptyResponseCodes.has(response.status) && emptyResponseValue !== undefined) {
+                        resolve(emptyResponseValue);
+                    } else {
+                        reject(new errorClass(messagePrefix, response, responseData));
+                    }
+                } else {
+                    resolve(responseData);
+                }
+            })
+            .catch((error) => {
+                // AbortError can be handled separately if desired.
+                reject(new errorClass(messagePrefix, error, {}));
+            });
+    });
+
+    // Attach a cancel method to the returned promise.
+    promise.cancel = () => {
+        controller.abort();
+    };
+
+    return promise;
 };
