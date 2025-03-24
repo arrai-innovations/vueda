224a2af291b0413559533e40593dcfebb5795be0
Peter Mack <peter@arrai.com>
2025-03-24 09:07:36 -0600

    chore: lint and address sorting issues

--- server/vueda/update.py
+++ server/vueda/update.py
@@ -6,12 +6,12 @@ import sys
 import typing
 from collections import OrderedDict
 from contextlib import contextmanager
-from test.support.os_helper import EnvironmentVarGuard
 from traceback import format_exception
 
 from argparse_color_formatter import ColorHelpFormatter
 from argparse_color_formatter import ColorTextWrapper
 from django.conf import settings
+from test.support.os_helper import EnvironmentVarGuard
 
 from vueda.cli import NoExitArgumentParser
 from vueda.cli import blue_color
