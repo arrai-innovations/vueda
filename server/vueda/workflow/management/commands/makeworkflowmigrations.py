7ed957397f5845660b84c838878ea5357aa2050d
Jayden Kneller <jayden@arrai.com>
2025-04-03 13:16:51 -0600

    fix: filter the history records by last migrated date, so we don't make history for old entries.

--- server/vueda/workflow/management/commands/makeworkflowmigrations.py
+++ server/vueda/workflow/management/commands/makeworkflowmigrations.py
@@ -979,6 +979,9 @@ def get_history_diff_other_models(historical_queryset, last_migrated_date, last_
                 history_date__gt=old_history_record.history_date,
             )
 
+        if last_migrated_date:
+            old_history_records_by_pk = old_history_records_by_pk.filter(history_date__gte=last_migrated_date)
+
         previous_history_record = old_history_record
         for history_record in old_history_records_by_pk.order_by("history_date"):
             history_type, history_diff = get_history_diff(previous_history_record, history_record)
