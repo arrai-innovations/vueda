2ca90cb9e7689bec6d5e6a435acbf030921b4c5b
Joel Hillacre <joel@403forbidden.ca>
2025-04-08 16:32:29 -0600

    fix: silence warnings, re: changing eslint ignored files getting passed by lint-staged

--- client/lint-staged.config.js
+++ client/lint-staged.config.js
@@ -1,5 +1,8 @@
 export default {
-    "**/*.{vue,js,cjs,mjs,ts,jsx,tsx}": ["npx --no-install eslint --cache --fix", "npx --no-install prettier --write"],
+    "**/*.{vue,js,cjs,mjs,ts,jsx,tsx}": [
+        "npx --no-install eslint --no-warn-ignored --cache --fix",
+        "npx --no-install prettier --write",
+    ],
     "**/*.{markdown,md}": ["npx --no-install doctoc --github -u --notitle"],
     "**/*.{less,scss,css,vue,markdown,json,md,yml,yaml,html}": ["npx --no-install prettier --write"],
     ".circleci/config.yml": ["circleci config validate"],
