0c41e1f40c2e2c13717f6fec3919189821ff2c52
Joel Hillacre <joel@403forbidden.ca>
2025-04-28 00:48:30 -0600

    feat: add UniqueTogetherWithPkValidator to surface conflicting PKs in validation errors

---
+++ server/vueda/core/validators.py
@@ -0,0 +1,29 @@
+from rest_framework.validators import UniqueTogetherValidator
+
+from vueda.core.exceptions import VuedaValidationError
+
+
+class UniqueTogetherWithPkValidator(UniqueTogetherValidator):
+    """
+    A validator like UniqueTogetherValidator, but returns the pk of the conflicting object
+    in the ValidationError.
+    """
+
+    def __call__(self, attrs, serializer):
+        queryset = self.queryset
+        filter_kwargs = {field_name: attrs[field_name] for field_name in self.fields}
+
+        instance = getattr(serializer, "instance", None)
+        conflicting_obj = queryset.filter(**filter_kwargs).first()
+
+        if conflicting_obj and (instance is None or conflicting_obj.pk != instance.pk):
+            raise VuedaValidationError(
+                {
+                    "non_field_errors": [
+                        {
+                            "detail": self.message,
+                            "conflicting_pk": conflicting_obj.pk,
+                        }
+                    ]
+                }
+            )
