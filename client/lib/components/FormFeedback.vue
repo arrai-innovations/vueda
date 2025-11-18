0396033117852894fd8af087e86a99dc4856c97e
Joel Hillacre <joel@403forbidden.ca>
2025-11-18 15:30:45 -0700

    chore(eslint): fix shadowed variable

--- client/lib/components/FormFeedback.vue
+++ client/lib/components/FormFeedback.vue
@@ -136,8 +136,8 @@ const renderDetail = (data) => {
                             <template v-if="isObject(line)">
                                 <!-- eslint-disable-next-line vue/no-v-html -->
                                 <div v-if="allowHtml" v-html="renderDetail(line)"></div>
-                                <div v-for="[name, message] of Object.entries(line)" v-else :key="name">
-                                    {{ name }}: {{ message }}
+                                <div v-for="[name, value] of Object.entries(line)" v-else :key="name">
+                                    {{ name }}: {{ value }}
                                 </div>
                             </template>
                             <template v-else-if="allowHtml && containsHtml(line)">
