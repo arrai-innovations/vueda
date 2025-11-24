d0aad3cae5a048c69f075e7e5faa8d436823114a
Jayden Kneller <jayden@arrai.com>
2025-11-24 15:17:08 -0700

    test: copy django testing code

--- server/tests/utils.py
+++ server/tests/utils.py
@@ -1,7 +1,14 @@
 import os
+import shutil
+import tempfile
 from contextlib import contextmanager
+from importlib import import_module
 
+from django.apps import apps
 from django.http import QueryDict
+from django.test import override_settings
+from django.test.utils import extend_sys_path
+from django.utils.module_loading import module_dir
 
 
 def clean_migrations(which_app=None):
@@ -74,6 +81,58 @@ def clean_migrations(which_app=None):
             for filename in files:
                 if filename not in existing_migrations:
                     os.remove(os.path.join(root, filename))
+
+
+class BaseTestMigrations:
+    # Copied from django with no changes:
+    # https://github.com/django/django/blob/14fb36e0b083ea963220602d01386cc0fb2c40e4/django/test/testcases.py#L393-L398
+    def settings(self, **kwargs):
+        """
+        A context manager that temporarily sets a setting and reverts to the
+        original value when exiting the context.
+        """
+        return override_settings(**kwargs)
+
+    # Copied from django with no changes:
+    # //github.com/django/django/blob/14fb36e0b083ea963220602d01386cc0fb2c40e4/tests/migrations/test_base.py#L186-L222
+    @contextmanager
+    def temporary_migration_module(self, app_label="migrations", module=None):
+        """
+        Allows testing management commands in a temporary migrations module.
+
+        Wrap all invocations to makemigrations and squashmigrations with this
+        context manager in order to avoid creating migration files in your
+        source tree inadvertently.
+
+        Takes the application label that will be passed to makemigrations or
+        squashmigrations and the Python path to a migrations module.
+
+        The migrations module is used as a template for creating the temporary
+        migrations module. If it isn't provided, the application's migrations
+        module is used, if it exists.
+
+        Returns the filesystem path to the temporary migrations module.
+        """
+        with tempfile.TemporaryDirectory() as temp_dir:
+            target_dir = tempfile.mkdtemp(dir=temp_dir)
+            with open(os.path.join(target_dir, "__init__.py"), "w"):
+                pass
+            target_migrations_dir = os.path.join(target_dir, "migrations")
+
+            if module is None:
+                module = apps.get_app_config(app_label).name + ".migrations"
+
+            try:
+                source_migrations_dir = module_dir(import_module(module))
+            except (ImportError, ValueError):
+                pass
+            else:
+                shutil.copytree(source_migrations_dir, target_migrations_dir)
+
+            with extend_sys_path(temp_dir):
+                new_module = os.path.basename(target_dir) + ".migrations"
+                with self.settings(MIGRATION_MODULES={app_label: new_module}):
+                    yield target_migrations_dir
 
 
 class FakeRequest:
