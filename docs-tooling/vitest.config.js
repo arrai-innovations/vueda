823e3371dacfe1d7aeffe756fb1362c2f0dc35a4
Joel Hillacre <joel@403forbidden.ca>
2026-03-02 16:58:50 -0700

    ci(docs-tooling): split test job and add coverage reporting

--- docs-tooling/vitest.config.js
+++ docs-tooling/vitest.config.js
@@ -3,5 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         include: ["tests/js/**/*.test.js", "tests/js/**/*.spec.js"],
+        coverage: {
+            reportOnFailure: true,
+            provider: "istanbul",
+            all: true,
+            include: ["js/**/*.js", "bin/docs-tooling.js"],
+            reporter: ["text", "json-summary", "html", "lcov"],
+        },
     },
 });
