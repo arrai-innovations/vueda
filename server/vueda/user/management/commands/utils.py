e7dace4a2d672a0dd1d4a75eb1ec6a41f58f7e34
Jayden Kneller <jayden@arrai.com>
2026-04-22 15:05:03 -0600

    fix: update docstring

--- server/vueda/user/management/commands/utils.py
+++ server/vueda/user/management/commands/utils.py
@@ -7,7 +7,7 @@ __all__ = (
 
 
 def get_matching_record(change, group_change_model=None):
-    """Return True if a GroupChange record matching this change exists, False otherwise."""
+    """Return GroupChange.pk if a record matching this change exists, None otherwise."""
     # Provide a way to use the model through a migration.
     if group_change_model is None:
         from vueda.user.models import GroupChange
