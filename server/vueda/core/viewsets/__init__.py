daf3d0004ed89b7dd7d3f691e1c55bbe81b62995
Jayden Kneller <jayden@arrai.com>
2026-05-11 15:45:22 -0600

    fix: evaluate get_queryset if needed

--- server/vueda/core/viewsets/__init__.py
+++ server/vueda/core/viewsets/__init__.py
@@ -647,7 +647,7 @@ class VuedaViewSet(FlexFieldsMixin, NoExtraFieldsForViewSetMixin, ListRowLevelVi
         if hasattr(self, "kwargs") and "pk" in self.kwargs:
             has_composite_primary_key = False
             cpk_field = None
-            model = getattr(self.queryset, "model", self.get_queryset().model)
+            model = getattr(self.queryset, "model", None) or self.get_queryset().model
             for field in model._meta.fields:
                 if isinstance(field, CompositePrimaryKey):
                     has_composite_primary_key = True
