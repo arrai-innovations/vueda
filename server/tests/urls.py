3468225763697444a8f903a81408524efdbfe605
Jayden Kneller <jayden@arrai.com>
2026-06-25 14:52:46 -0600

    remove: unused router

--- server/tests/urls.py
+++ server/tests/urls.py
@@ -5,6 +5,5 @@ from django.urls import path
 urlpatterns = [
     path("", include("tests.routers")),
     path("store/", include("tests.store.routers")),
-    path("logging/", include("tests.logging.routers")),
     path("confirmation/", include("tests.confirmation.routers")),
 ]
