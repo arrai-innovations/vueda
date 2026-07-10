781ffd71df1929e544a927ee7a8dfbc930ca937e
Jayden Kneller <jayden@arrai.com>
2026-07-10 10:28:11 -0600

    fix: shorten index names

--- server/tests/group_updating_no_migrations/models.py
+++ server/tests/group_updating_no_migrations/models.py
@@ -24,6 +24,6 @@ class GroupUpdatingNoMigrationsUser(AbstractVUEDAUser):
     class Meta(AbstractVUEDAUser.Meta):
         default_related_name = "group_updating_no_migrations_users"
         indexes = [
-            GinIndex(fields=["email"], name="gin_email_group_updating_no_migrations_idx", opclasses=["gin_trgm_ops"]),
-            GinIndex(fields=["name"], name="gin_name_group_updating_no_migrations_idx", opclasses=["gin_trgm_ops"]),
+            GinIndex(fields=["email"], name="gin_email_grp_up_no_mgr_idx", opclasses=["gin_trgm_ops"]),
+            GinIndex(fields=["name"], name="gin_name_grp_up_no_mgr_idx", opclasses=["gin_trgm_ops"]),
         ]
