89f6fc7482c41c042dafe9eb3c217051b8d46ce6
Zoey Zhou <zoey@arrai.com>
2026-08-20 12:04:12 -0600

    chore: removed the dead default_type constant from VuedaValidationError

--- server/vueda/core/exceptions.py
+++ server/vueda/core/exceptions.py
@@ -195,8 +195,6 @@ def _get_error_details(data, default_code=None):
 
 
 class VuedaValidationError(ValidationError):
-    default_type = "error"
-
     def __init__(self, detail=None, code=None):
         if detail is None:
             detail = self.default_detail
