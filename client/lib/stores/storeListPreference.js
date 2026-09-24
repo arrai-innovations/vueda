16c49fb9fc2a5b3d9d10ca80ef55e27427bcaf7c
Zoey Zhou <zoey@arrai.com>
2026-09-24 15:39:57 -0600

    chore: remove debug log

--- client/lib/stores/storeListPreference.js
+++ client/lib/stores/storeListPreference.js
@@ -124,7 +124,6 @@ export const storeListPreference = defineStore("listPreference", {
         },
 
         setFilters(args, filters) {
-            console.log("filters", filters);
             const key = getAppModelDotName(args);
             const hasValue = filters && Object.keys(filters).length > 0;
             this._updatePreference(key, "filters", hasValue ? filters : undefined);
